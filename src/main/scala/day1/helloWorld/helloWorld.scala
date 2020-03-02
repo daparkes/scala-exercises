4a9e55bfe1957a735e91713a7335cd12b716370b
Dan Parkes <dan.parkes15@gmail.com>
2020-03-02 16:29:40 +0000

    added helloWorld solution

---
+++ src/main/scala/day1/helloWorld/helloWorld.scala
@@ -0,0 +1,12 @@
+package day1.helloWorld
+
+object helloWorld {
+  def helloWorld(): Unit = {
+    println("Hello World!")
+  }
+
+  def main(args: Array[String]): Unit = {
+    helloWorld()
+  }
+
+}
