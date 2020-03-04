984c12d69a36f1cbb592be72d48c1d18ece08025
Dan Parkes <dan.parkes15@gmail.com>
2020-03-04 10:13:27 +0000

    working hangman

--- src/main/scala/day2/hangman/Hangman.scala
+++ src/main/scala/day2/hangman/Hangman.scala
@@ -5,12 +5,11 @@ import scala.util.Random._
 
 object Hangman {
   def chooseWord: String = {
-    val file = "/home/dan/Documents/scala-exercises/src/main/scala/day2/hangman/enable1.txt"
+    val file = "/home/dan/IdeaProjects/scala-exercises/src/main/scala/day2/hangman/enable1.txt"
     val fileContents = Source.fromFile(file)
     val wordList = fileContents.getLines.toList
-    val randWord = wordList(nextInt(wordList.length))
     fileContents.close
-    randWord
+    wordList(nextInt(wordList.length))
   }
 
   def drawHangman(lives: Int): Unit = {
@@ -64,56 +63,54 @@ object Hangman {
         "   |        \n" +
         "______      \n" +
         "  |  | ")
-      println("Game over!")
     }
   }
 
   def getInput: Char = {
     var letter = StdIn.readLine("Guess a letter: ")
-    var charLetter = letter(0)
-    charLetter
+    letter(0)
   }
 
   def main(args: Array[String]): Unit = {
     val word = chooseWord
-    var correctlyGuessedLetters = List()
+    println(word)
+    var correctlyGuessedLetters = ""
     var lives = 5
     var currentGuess = ""
     var continue = true
 
     while (continue) {
+
       var playerGuess = getInput
       if (word.contains(playerGuess)) {
         println("That's correct!")
-        currentGuess += playerGuess
-        for (let <- word) {
-          for (guessedLet <- correctlyGuessedLetters) {
-            if (guessedLet == let) {
-              currentGuess += guessedLet
-              correctlyGuessedLetters :+ guessedLet
-            }
-            else {
-              currentGuess += "_"
-            }
-          }
-        }
-        println(currentGuess)
-      }
-      else {
+        correctlyGuessedLetters += playerGuess
+      } else {
         println("Wrong!")
-        currentGuess += "_"
         lives -= 1
         drawHangman(lives)
-        if (lives == 0) {
-          continue = false
-          println("You fail")
-        }
       }
-    }
 
-    if (currentGuess.equals(word)) {
-      println("YOU WIN HOORAY")
-      continue = false
+      currentGuess = ""
+      for (let <- word) {
+        if (correctlyGuessedLetters.contains(let)){
+          currentGuess += let
+        } else {
+          currentGuess += "_"
+          }
+        }
+
+      println(currentGuess)
+
+      if (lives == 0) {
+        continue = false
+        println("You fail")
+      }
+
+      if (currentGuess.equals(word)) {
+        println("YOU WIN HOORAY")
+        continue = false
+      }
     }
   }
 }
