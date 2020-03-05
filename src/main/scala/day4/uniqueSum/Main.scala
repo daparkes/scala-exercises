e8a65b1ed67be250f8ea497222c64770a50ba537
Dan Parkes <dan.parkes15@gmail.com>
2020-03-05 22:21:41 +0000

    tooHot solution

--- src/main/scala/day4/uniqueSum/Main.scala
+++ src/main/scala/day4/uniqueSum/Main.scala
@@ -1,9 +1,9 @@
 package day4.uniqueSum
 
 object Main extends App {
-  def uniqueSum(nums: Array[Int]): Int = {
+  def uniqueSum(nums: Array[Int]): Unit = {
     nums.groupBy(x=>x).filter(_._2.lengthCompare(1) == 0).keySet.sum
   }
 
-  println(uniqueSum(Array(1, 5, 2)))
+  uniqueSum(Array(1, 2, 2))
 }
