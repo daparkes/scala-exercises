a48354fee351b562d561bc4a415ac9bd426274a8
Dan Parkes <dan.parkes15@gmail.com>
2020-03-11 21:14:00 +0000

    added wrong guesses list and try-catch for getInput

---
+++ src/main/scala/day2/hangman/Game.scala
@@ -0,0 +1,62 @@
+package day2.hangman
+
+import scala.io.{Source, StdIn}
+import scala.util.Random.nextInt
+
+object Game {
+  def chooseWord: String = {
+    val file = "enable1.txt"
+    val fileContents = Source.fromFile(file)
+    val wordList = fileContents.getLines.toList
+    fileContents.close
+    wordList(nextInt(wordList.length))
+  }
+
+  def drawHangman(mistakes: Int): Unit = {
+    val hangman = Array(
+      "   _____   \n",
+      "   |    |    \n",
+      "   |    0    \n",
+      "   |   /|\\  \n",
+      "   |   / \\  \n",
+      "   |         \n",
+      "______       \n",
+      "  |  | ")
+    for (i <- hangman.length-mistakes until hangman.length) {
+      print(hangman(i))
+    }
+  }
+
+  @scala.annotation.tailrec
+  def getInput: Char = {
+    try {
+      StdIn.readLine("Guess a letter: ")(0)
+    } catch {
+      case e: StringIndexOutOfBoundsException =>
+        println("You need to enter a letter!")
+        getInput
+    }
+  }
+
+  def buildCurrentGuess(word: String, correctlyGuessedLetters: String): String = {
+    var currentGuess = ""
+    println()
+    word.foreach(let =>
+      if (correctlyGuessedLetters.contains(let)){
+        currentGuess += let
+      } else {
+        currentGuess += "_"
+      }
+    )
+    currentGuess
+  }
+
+  def printWrongGuesses(wrongLetters: String): Unit = {
+    println("Wrong guesses:")
+    wrongLetters.foreach(e =>
+      print(s"$e ")
+    )
+    println()
+  }
+
+}
--- src/main/scala/day2/hangman/Hangman.scala
+++ src/main/scala/day2/hangman/Hangman.scala
@@ -1,82 +1,41 @@
 package day2.hangman
 
-import scala.collection.mutable.ArrayBuffer
-import scala.io.{Source, StdIn}
-import scala.util.Random._
+import Game._
 
 object Hangman {
-  def chooseWord: String = {
-    val file = "enable1.txt"
-    val fileContents = Source.fromFile(file)
-    val wordList = fileContents.getLines.toList
-    fileContents.close
-    wordList(nextInt(wordList.length))
-  }
-
-  def drawHangman(mistakes: Int): Unit = {
-    val hangman = Array(
-      "   _____   \n",
-    "   |    |    \n",
-    "   |    0    \n",
-    "   |   /|\\  \n",
-    "   |   / \\  \n",
-    "   |         \n",
-    "______       \n",
-    "  |  | ")
-    for (i <- hangman.length-mistakes until hangman.length) {
-      print(hangman(i))
-    }
-  }
-  def getInput: Option[Char] = {
-    try {
-      Some(StdIn.readLine("Guess a letter: ")(0))
-    } catch {
-      case e: StringIndexOutOfBoundsException => None
-    }
-  }
-
-  def buildCurrentGuess(word: String, correctlyGuessedLetters: String): String = {
-    var currentGuess = ""
-    println()
-    word.foreach(let =>
-      if (correctlyGuessedLetters.contains(let)){
-        currentGuess += let
-      } else {
-        currentGuess += "_"
-      }
-    )
-    currentGuess
-  }
 
   def main(args: Array[String]): Unit = {
     val word = chooseWord
-
+    println(word)
     var correctlyGuessedLetters = ""
     var mistakes = 0
-    var currentGuess = ""
     var continue = true
+    var wrongLetters = ""
 
     while (continue) {
-
-      var playerGuess = getInput
-      if (playerGuess.isDefined) {
+      val playerGuess = getInput
+      if (playerGuess.toString.length > 0) {
         if (word.contains(playerGuess)) {
           println("That's correct!")
           correctlyGuessedLetters += playerGuess
+          println(correctlyGuessedLetters)
         } else {
           println("Wrong!")
           mistakes += 1
+          wrongLetters += playerGuess
           drawHangman(mistakes)
         }
       }
 
-      println(buildCurrentGuess(word, correctlyGuessedLetters))
+      val currentGuess = buildCurrentGuess(word, correctlyGuessedLetters)
+      println(currentGuess)
+
+      printWrongGuesses(wrongLetters)
 
       if (mistakes == 8) {
         continue = false
         println("You fail")
       }
-
       if (currentGuess.equals(word)) {
         println("YOU WIN HOORAY")
         continue = false
